7eb3533b45ee31d85d3fdca6d6582ac764c2190f
Absolom Mugwagwa <absolom@Werner-Terblanche.local>
2019-07-25 14:51:20 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,20 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <LightBox :images="images"></LightBox>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import LightBox from 'vue-image-lightbox'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    LightBox,
+       
   }
 }
 </script>
