90e94f391d72c6969936d8a876332e447dace481
Dominic Lee <domlee99c@gmail.com>
2019-08-08 13:00:09 +0200

    Sorting added

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app" class="container">
     <div v-for="(event, eIndex) in events.gallery" :key="event.name">
-      <div class="gallery-header" v-if="event.items.length"><h3 >{{ event.name }}</h3><div class="view-all" @click="index = 0; eventIndex = eIndex">View all ({{event.items.length}})</div></div>
+      <div class="gallery-header" v-if="event.items.length"><h3>{{ event.name }}</h3><div class="view-all" @click="index = 0; eventIndex = eIndex">View all ({{event.items.length}})</div></div>
       <div v-if="event.items.length" class="event-container">
         <div class="image-container" v-for="(image, i) in event.items.slice(0,maxItems)" :key="i" @click="index = i; eventIndex = eIndex">
           <img class="image" :src="image.filename">
@@ -36,19 +36,35 @@
       this.maxItems = Modernizr && Modernizr.touchevents && Modernizr.mq('(max-width: 460px)') ? 2 : 6;
     },
     mounted() {
+      let allPromises = [];
       axios.get(`http://ec2-54-161-60-4.compute-1.amazonaws.com/api/events`)
       .then(response => {
         response.data.forEach((item) => {
-          let galleryItem = {name: item.event_name, items: []};
+          let galleryItem = {name: item.event_name, id: item.id, created: item['created_at'], items: []};
           let url = `http://ec2-54-161-60-4.compute-1.amazonaws.com/api/events/${item.id}`;
-          axios.get(url).then(response => {
-            galleryItem.items = response.data.map((_entry) => {
-              _entry.filename = 'http://ec2-54-161-60-4.compute-1.amazonaws.com/uploads/' + _entry.filename;
-              return _entry
-            });
-            this.events.gallery.push(galleryItem);
-          })
+          let getPromise = axios.get(url);
+          allPromises.push(getPromise);
+          this.events.gallery.push(galleryItem);
         });
+
+        Promise.all(allPromises).then((_galleries) => {
+          _galleries.forEach((_gallery, _index) => {
+            try {
+              _gallery.data.forEach(_entry => {
+                _entry.filename = 'http://ec2-54-161-60-4.compute-1.amazonaws.com/uploads/' + _entry.filename;
+              });
+              this.events.gallery[_gallery.data[0]['event_id'] - 1].items = _gallery.data;
+            } catch(e) {
+              // No entries
+            }
+          });
+          // Sort
+          this.events.gallery = this.events.gallery.sort((a,b) => {
+            let aDate = new Date(a.created);
+            let bDate = new Date(b.created);
+            return aDate < bDate ? 1 : -1;
+          });
+        })
       })
     },
   }
