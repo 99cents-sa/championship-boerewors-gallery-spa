e424dfdc39a32e5e2a0d9b98d1950f35101fd296
Absolom Mugwagwa <absolom@Werner-Terblanche.local>
2019-08-08 15:25:59 +0200

    change div id from app to cqcanvas

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+
+import Vue2Filters from 'vue2-filters'
+Vue.use(Vue2Filters)
+
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#cqcanvas')
